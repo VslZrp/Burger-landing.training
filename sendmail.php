fa7031d2ff550b4538f2bbb8ceedc045fd5230db
VslZrp <133650026+VslZrp@users.noreply.github.com>
2023-05-31 22:01:07 +0400

    Add files via upload

--- sendmail.php
+++ sendmail.php
@@ -14,15 +14,18 @@ $mail->setFrom('pinokkio91@gmail.com', 'Vaso ski boom');
 $mail->addAddress('vslzrp@i.ua');
 $mail->Subject = 'Hello! It is the order for burgers';
 
-$body = '<h1>Gamarjoba, genatsvale!</h1>'
+$body = '<h1>Gamarjoba, genatsvale!</h1>';
+
 if (trim(!empty($_POST['name']))){
     $body.='<p><strong>Name: </strong> '.$_POST['name'].'</p>';
 }
 if(trim(!empty($_POST['tel']))){
     $body.='<p><strong>Tel: </strong> '.$_POST['tel'].'</p>';
-}if(trim(!empty($_POST['email']))){
+}
+if(trim(!empty($_POST['email']))){
     $body.='<p><strong>E-Mail: </strong> '.$_POST['email'].'</p>';
-}if(trim(!empty($_POST['order']))){
+}
+if(trim(!empty($_POST['order']))){
     $body.='<p><strong>Order: </strong> '.$_POST['order'].'</p>';
 }
 
