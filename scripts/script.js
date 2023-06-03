6d410b3c0eb42af4bdfc5602f6c3a46c51648f0c
VslZrp <133650026+VslZrp@users.noreply.github.com>
2023-06-03 09:13:13 +0400

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -36,6 +36,13 @@ const iconMenu = document.querySelector(".menu__icon");
 const menuBody = document.querySelector(".header__menu");
 const menuLinks = document.querySelectorAll("a[data-goto]");
 const mainPageButtons = document.querySelectorAll(".text-main__button");
+const currency = document.querySelector(".header__button");
+const phoneInput = document.querySelector("input[type=tel]");
+
+//==========Поправка для корректного добавления в корзину==========================//
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".menu__items_1").classList.remove("_closed");
+});
 
 //==============Burger menu=================================================//
 
@@ -102,7 +109,6 @@ if (mainPageButtons.length > 0) {
   }
 }
 //===============Change Currency=========================================================================================//
-const currency = document.querySelector(".header__button");
 currency.addEventListener("click", function (e) {
   const currentCurrency = currency.innerText;
   if (currentCurrency === "$") {
@@ -114,6 +120,26 @@ currency.addEventListener("click", function (e) {
   }
   document.querySelector(".order-form__actual-currency").textContent =
     currency.innerText;
+
+  //==========Change of total price currency=====================================================//
+
+  const totalPrice = document.querySelector(".order-form__total-sum");
+  if (currency.innerText === "$") {
+    totalPrice.textContent = ` ${(
+      (totalPrice.textContent * usdCoefficient) /
+      uahCoefficient
+    ).toFixed(0)}`;
+  } else if (currency.innerText === "€") {
+    totalPrice.textContent = ` ${(
+      (totalPrice.textContent * euroCoefficient) /
+      usdCoefficient
+    ).toFixed(1)}`;
+  } else if (currency.innerText === "₴") {
+    totalPrice.textContent = ` ${(
+      (totalPrice.textContent * uahCoefficient) /
+      euroCoefficient
+    ).toFixed(0)}`;
+  }
 });
 
 //=================CHANGE PRICES============================================//
@@ -143,7 +169,7 @@ for (let i = 0; i < prices.length; i++) {
       ).toFixed(0)}`;
     }
   }
-  //============Total Price===================================================//
+  //============Burger Price===================================================//
   price.textContent = `${price.textContent} ${currency.innerText}`;
   currency.addEventListener("click", function (e) {
     if (currency.innerText === "$") {
@@ -194,6 +220,12 @@ for (let i = 0; i < buttonsOrderBurger.length; i++) {
       e.preventDefault();
     });
     //------------------------------------------//
+    const removedInputs = document.querySelectorAll(
+      ".order-form__your-order-items"
+    );
+    if (removedInputs.length === 0) {
+      j = 0;
+    }
     if (j === 0) {
       const input = document.createElement("input");
       input.type = "text";
@@ -216,30 +248,43 @@ for (let i = 0; i < buttonsOrderBurger.length; i++) {
       }
     }
     j++;
+
+    //====================ADD Burger Name Copy When Order=========================================================//
+    if (document.querySelector("._addedBurger")) {
+      document
+        .getElementById("temporaryCartLink")
+        .addEventListener("click", (event) => {
+          event.preventDefault();
+        });
+
+      document.querySelector("._addedBurger").remove();
+    }
+    const addedBurger = document.createElement("div");
+    addedBurger.className = "_addedBurger";
+    addedBurger.innerHTML = `<span>${nameOfBurger}</span> добавлен в <a href="" data-goto=".order-form" id="temporaryCartLink">корзину</a>`;
+    document.querySelector("body").append(addedBurger);
+
+    setTimeout(() => {
+      addedBurger.remove();
+    }, 3000);
+    //===================Go To Cart By Temporary Link=======================================//
+
+    const temporaryCartLink = document.getElementById("temporaryCartLink");
+    temporaryCartLink.addEventListener("click", onTemporaryLinkClick);
+    function onTemporaryLinkClick(e) {
+      const gotoBlock = document.querySelector(temporaryCartLink.dataset.goto);
+      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY;
+
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: "smooth",
+      });
+      e.preventDefault();
+    }
   }
 }
-//==========Change of total price currency=====================================================//
-currency.addEventListener("click", function (e) {
-  const totalPrice = document.querySelector(".order-form__total-sum");
-  if (currency.innerText === "$") {
-    totalPrice.textContent = ` ${(
-      (totalPrice.textContent * usdCoefficient) /
-      uahCoefficient
-    ).toFixed(0)}`;
-  } else if (currency.innerText === "€") {
-    totalPrice.textContent = ` ${(
-      (totalPrice.textContent * euroCoefficient) /
-      usdCoefficient
-    ).toFixed(1)}`;
-  } else if (currency.innerText === "₴") {
-    totalPrice.textContent = ` ${(
-      (totalPrice.textContent * uahCoefficient) /
-      euroCoefficient
-    ).toFixed(0)}`;
-  }
-});
+
 //=================ONLY DIGITS TO INPUT======================================================//
-const phoneInput = document.querySelector("input[type=tel]");
 phoneInput.addEventListener("keyup", function (event) {
   this.value = this.value.replace(/[^\w.-]|[a-zA-Z]|^[.-]/g, "");
 });
@@ -252,10 +297,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let error = formValidate(form);
 
-    let formData = new formData(form);
+    //    let formData = new formData(form);
 
     if (error === 0) {
-      let response = await fetch("sendmail.php", {
+      const removedInputs = document.querySelectorAll(
+        ".order-form__your-order-items"
+      );
+      for (let i = 0; i < removedInputs.length; i++) {
+        removedInputs[i].outerHTML = "";
+      }
+      totalPrice.textContent = "0";
+
+      successfulOrder();
+      form.reset();
+      /*      let response = await fetch("sendmail.php", {
         method: "POST",
         body: FormData,
       });
@@ -269,9 +324,9 @@ document.addEventListener("DOMContentLoaded", function () {
       } else {
         alert("Error");
         form.classList.remove("_sending");
-      }
+      }*/
     } else {
-      alert("Заполните все обязательные поля");
+      emptyInputsError();
     }
   }
 
@@ -301,7 +356,7 @@ document.addEventListener("DOMContentLoaded", function () {
       document
         .querySelector(".order-form__your-order-text-block")
         .classList.add("_error");
-      alert("Ваша корзина пуста :(");
+      emptyCartError();
       error++;
     }
     return error;
@@ -321,3 +376,171 @@ document.addEventListener("DOMContentLoaded", function () {
     );
   }
 });
+
+//====================Next Menu Page============================================================================//
+
+const nextPageMenuButton = document.querySelector(
+  ".pages-menu__next-page-button"
+);
+nextPageMenuButton.addEventListener("click", function (e) {
+  const menuPages = document.querySelectorAll(".menu__items");
+  for (let i = 0; i < menuPages.length - 1; i++) {
+    if (!menuPages[i].classList.contains("_closed")) {
+      const nextPage = menuPages[i].nextElementSibling;
+      menuPages[i].classList.toggle("_closed");
+      nextPage.classList.toggle("_closed");
+
+      //----------Box Shadow for Open Page----------------------------//
+
+      const allDigitsPages = document.querySelectorAll(".pages-menu__button");
+      for (let j = 0; j < allDigitsPages.length; j++) {
+        if (allDigitsPages[j].classList.contains("_open")) {
+          allDigitsPages[j].classList.remove("_open");
+        }
+        if (!menuPages[j].classList.contains("_closed")) {
+          allDigitsPages[j].classList.add("_open");
+        }
+      }
+      //--------------------------//
+      break;
+    }
+  }
+  e.preventDefault;
+});
+//=============Previous Menu Page===========================================================================================================================//
+const previousPageMenuButton = document.querySelector(
+  ".pages-menu__previous-page-button"
+);
+previousPageMenuButton.addEventListener("click", function (e) {
+  const menuPages = document.querySelectorAll(".menu__items");
+  for (let i = 1; i < menuPages.length; i++) {
+    if (!menuPages[i].classList.contains("_closed")) {
+      const previousPage = menuPages[i].previousElementSibling;
+      menuPages[i].classList.toggle("_closed");
+      previousPage.classList.toggle("_closed");
+      //----------Box Shadow for Open Page----------------------------//
+
+      const allDigitsPages = document.querySelectorAll(".pages-menu__button");
+      for (let j = 0; j < allDigitsPages.length; j++) {
+        if (allDigitsPages[j].classList.contains("_open")) {
+          allDigitsPages[j].classList.remove("_open");
+        }
+        if (!menuPages[j].classList.contains("_closed")) {
+          allDigitsPages[j].classList.add("_open");
+        }
+      }
+      //--------------------------------//
+      break;
+    }
+  }
+  e.preventDefault;
+});
+
+//====================Go To Choosed Page Of Burger Menu================================================================//
+//----------PAGE1----------------------------//
+const page1Button = document.getElementById("pageMenuButton1");
+page1Button.addEventListener("click", function (e) {
+  const allMenuPages = document.querySelectorAll(".menu__items");
+  for (let i = 0; i < allMenuPages.length; i++) {
+    if (!allMenuPages[i].classList.contains("_closed")) {
+      allMenuPages[i].classList.add("_closed");
+    }
+  }
+  const menuPage1 = document.querySelector(".menu__items_1");
+  menuPage1.classList.remove("_closed");
+
+  const allDigitsPages = document.querySelectorAll(".pages-menu__button");
+  for (let i = 0; i < allDigitsPages.length; i++) {
+    if (allDigitsPages[i].classList.contains("_open")) {
+      allDigitsPages[i].classList.remove("_open");
+    }
+  }
+  page1Button.classList.add("_open");
+});
+//----------PAGE1----------------------------//
+const page2Button = document.getElementById("pageMenuButton2");
+page2Button.addEventListener("click", function (e) {
+  const allMenuPages = document.querySelectorAll(".menu__items");
+  for (let i = 0; i < allMenuPages.length; i++) {
+    if (!allMenuPages[i].classList.contains("_closed")) {
+      allMenuPages[i].classList.add("_closed");
+    }
+  }
+  const menuPage2 = document.querySelector(".menu__items_2");
+  menuPage2.classList.remove("_closed");
+
+  const allDigitsPages = document.querySelectorAll(".pages-menu__button");
+  for (let i = 0; i < allDigitsPages.length; i++) {
+    if (allDigitsPages[i].classList.contains("_open")) {
+      allDigitsPages[i].classList.remove("_open");
+    }
+  }
+  page2Button.classList.add("_open");
+});
+//----------PAGE1----------------------------//
+const page3Button = document.getElementById("pageMenuButton3");
+page3Button.addEventListener("click", function (e) {
+  const allMenuPages = document.querySelectorAll(".menu__items");
+  for (let i = 0; i < allMenuPages.length; i++) {
+    if (!allMenuPages[i].classList.contains("_closed")) {
+      allMenuPages[i].classList.add("_closed");
+    }
+  }
+  const menuPage3 = document.querySelector(".menu__items_3");
+  menuPage3.classList.remove("_closed");
+
+  const allDigitsPages = document.querySelectorAll(".pages-menu__button");
+  for (let i = 0; i < allDigitsPages.length; i++) {
+    if (allDigitsPages[i].classList.contains("_open")) {
+      allDigitsPages[i].classList.remove("_open");
+    }
+  }
+  page3Button.classList.add("_open");
+});
+
+//=========Всплывающее окно при успешном заказе====================================//
+function successfulOrder() {
+  const orderSuccess = document.createElement("div");
+  orderSuccess.className = "order__success";
+  orderSuccess.textContent =
+    "Спасибо за заказ! Наш менеджер скоро свяжется с Вами";
+  document.querySelector("body").append(orderSuccess);
+  setTimeout(() => {
+    orderSuccess.remove();
+  }, 5000);
+}
+
+//==================Error Empty Cart====================================//
+function emptyCartError() {
+  const emptyCartPopap = document.createElement("div");
+  emptyCartPopap.className = "emptyCartError";
+  emptyCartPopap.textContent = "Ваша корзина пуста :(";
+  document.querySelector("body").append(emptyCartPopap);
+  setTimeout(() => {
+    emptyCartPopap.remove();
+  }, 4000);
+}
+
+//====================Error Empty Inputs==================================//
+function emptyInputsError() {
+  const emptyInputsPopap = document.createElement("div");
+  emptyInputsPopap.className = "emptyInputsError";
+  emptyInputsPopap.textContent = "Заполните все обязательные поля";
+  document.querySelector("body").append(emptyInputsPopap);
+  setTimeout(() => {
+    emptyInputsPopap.remove();
+  }, 4000);
+}
+//====================================================================================================//
+const menuBurgerCards = document.querySelectorAll(".menu__item");
+for (let i = 1; i < menuBurgerCards.length; i++) {
+  if (menuBurgerCards[i].clientHeight < menuBurgerCards[i - 1].clientHeight) {
+    menuBurgerCards[i].style.height = menuBurgerCards[i - 1].clientHeight;
+  } else if (
+    menuBurgerCards[i].clientHeight > menuBurgerCards[i - 1].clientHeight
+  ) {
+    menuBurgerCards[i - 1].style.height = menuBurgerCards[i].clientHeight;
+  } else {
+    menuBurgerCards[i].style.height = menuBurgerCards[i].clientHeight;
+  }
+}
